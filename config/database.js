89adc1fb87434ec5b99f929c91c866264603dc7d
limali <43158379+limali@users.noreply.github.com>
2020-06-27 01:30:16 +0530

    vendor login and registration

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import constants from './constants';
+
+//connect the db with the url provide
+try {
+    mongoose.connect(constants.MONGO_URL);
+}
+catch (err) {
+    mongoose.createConnection(constants.MONGO_URL);
+}
+
+mongoose.connection
+    .once('open', () => console.log('MongoDB Running'))
+    .on('error', e => {
+        throw e;
+    });
