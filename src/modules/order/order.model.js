2b4820141605643bd40f2d8cc5f82bbcdcb2f2c1
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2020-08-08 12:37:57 +0530

    Update order.model.js

--- src/modules/order/order.model.js
+++ src/modules/order/order.model.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-
 const orderSchema = new mongoose.Schema({
-    products: {
-        type: Array,
-        required: true
-    },
-    description : {
-        type: String,
-        trim: true,
+  products: {
+    type: Array,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
 
     completed: {
       type: Boolean,
@@ -30,21 +29,20 @@ const orderSchema = new mongoose.Schema({
       required: true,
     },
 
-    longitude : {
-        type : Number,
-        required: true
+    longitude: {
+      type: Number,
+      required: true,
     },
     date: {
-        type: Date,
-        required: true
-    }
-},{
-    timestamps: true
-})
-
-const Order = mongoose.model('Order',orderSchema)
-
+      type: Date,
+      required: true,
+    },
+  },
+  timestamps: true,
+});
 
 const Order = mongoose.model("Order", orderSchema);
+
+// const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
