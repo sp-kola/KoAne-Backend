56b9abb8795bcd0607d85047ea42342bd6e10280
limali <43158379+limali@users.noreply.github.com>
2020-08-25 20:09:21 +0530

    Update vendor.controllers.js

--- src/modules/vendor/vendor.controllers.js
+++ src/modules/vendor/vendor.controllers.js
@@ -1,18 +1,18 @@
 import Vendor from './vendor.model';
-import User from '../user/user.model'
+import User from '../user/user.model';
 
 export async function signUp(req, res) {
     const data = {
         email: req.body.email,
-        password : req.body.password,
+        password: req.body.password,
         userName: req.body.userName,
         type: 'Vendor'
-    }
+    };
     try {
         const user = await User.create(data);
         const vendor = await Vendor.create({
             ...req.body,
-            userID : user._id
+            userID: user._id
         });
         return res.status(201).json(vendor);
     }
@@ -25,3 +25,4 @@ export function login(req, res, next) {
     res.status(200).json(req.vendor);
     return next();
 }
+
